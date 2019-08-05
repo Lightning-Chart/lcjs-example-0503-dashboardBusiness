7032063b51240f9a6bf448ee6e453279a228e7ad
Ilkka Kuosmanen <ilkka.kuosmanen@arction.com>
2019-08-05 13:41:58 +0300

    Initial

---
+++ README.md
@@ -0,0 +1,175 @@
+# Dashboard business
+
+This demo application belongs to the set of examples for LightningChart JS, data visualization library for JavaScript.
+
+LightningChart JS is entirely GPU accelerated and performance optimized charting library for presenting massive amounts of data. It offers an easy way of creating sophisticated and interactive charts and adding them to your website or web application.
+
+The demo can be used as an example or a seed project. Local execution requires the following steps:
+
+- Make sure that relevant version of [Node.js](https://nodejs.org/en/download/) is installed
+- Open the project folder in a terminal:
+
+        npm install              # fetches dependencies
+        npm start                # builds an application and starts the development server
+
+- The application is available at *http://localhost:8080* in your browser, webpack-dev-server provides hot reload functionality.
+
+### Description 
+
+The data visualization tools are widely used in all fields of industries. This example shows the specific business case to visualize the costs of the *imaginary company* across all the departments for the whole year combined in a single interactive dashboard.
+
+#### Dashboard layout
+
+The dashboard grid is created with 4 rows and 2 columns. Some visualization components in this example fill multiple cells by explicitly providing a row- & column- span during the creation.
+
+1. *Top-left cell.* The cell contains a chart that shows the costs per year for each department using ***Bar Chart or Column Chart*** implemented with ***SegmentSeries*** different tool than ***RectangleSeries***. Segment series provides an ability to create and place freely line segments by specifying the start & end.
+
+    ```javascript
+    // Create XY chart and attach to the dashboard.
+    const barChart = dashboard.createChartXY({
+    columnIndex: 2,
+    rowIndex: 0,
+    columnSpan: 2,
+    rowSpan: 1
+})
+
+    // Add segment series to series individual line segments.
+    // This series uses default axes.
+    const bars = barChart.addSegmentSeries()
+    ```
+    The segment series accepts input in the following format { startX: number, startY: number, endX: number, endY: number }. The series returns the created line segment to give an ability of further modifications. The chart fills 2 rows & 1 column.
+
+    ```javascript
+    // Add line segment.
+    const column = bars.add({
+        startX: 10,
+        startY: 10,
+        endX: 20,
+        endY: 20
+    })
+    ```
+
+    Regarding the customization, each column can be configured to have individual styling and mouse & touch events. Each chart and series has mouse/touch events. Search in our API documentation by starting to type "onMouse".
+
+    ```javascript
+    // Configure the created bar column.
+    column
+        .setStrokeStyle( style => ... )
+        .onMouseEnter( () => pointerEnterHandler )
+        .onTouchEvent( () => pointerEnterHandler )
+    ```
+
+2. *Top-right cell, upper part.* This cell contains a UI panel that shows the total company costs for the whole year. UI panels are able to visualize only UI components. The chart fills 1 row & 1 column.
+
+    ```javascript
+    // Create UI panel and attach to the dashboard.
+    const panel = dashboard.createUIPanel({
+    columnIndex: 3,
+    rowIndex: 1,
+    columnSpan: 1,
+    rowSpan: 1
+})
+
+    // Add UI element specifying the builder.
+    // E.g. CheckBox, Button, Legend, etc.
+    panel.addUIElement(/* builder from the library */)
+    ```
+
+ 3. *Top-right cell, lower part.* This cell contains a chart which shows the only costs for a single department for each day of the year. The chart renders the line series with data gathered by moving the mouse over the column in the bar chart. The chart fills 1 row & 1 column.
+
+    ```javascript
+    // Decide on an origin for DateTime axes.
+    const dateTimeTickStrategy = AxisTickStrategies.DateTime(new Date(2018, 0, 1))
+
+    // Create a chart for visualizing the costs of selected department.
+    // Specify DateTime format for x-axis labels.
+    const lineChart = dashboard.createChartXY({
+        columnIndex: 2, 
+        rowIndex: 1, 
+        columnSpan: 1, 
+        rowSpan:1, 
+        chartXYOptions: { defaultAxisXTickStrategy: dateTimeTickStrategy }
+    })
+
+    // Create line series for elected department.
+    const lineSeries = lineChart.addLineSeries()
+    ```
+
+4. *Bottom cell.* This cell contains a chart which shows the smooth line ( using ***SplineSeries*** ) of total costs across all the apartments during the whole year. The chart fills 2 rows & 2 columns.
+    ```javascript
+    // Create a chart for visualizing the total costs of the company.
+    // Specify DateTime format for x-axis labels similarly as before.
+    const totalCostsChart = dashboard.createChartXY({
+        columnIndex: 0, 
+        rowIndex: 0, 
+        columnSpan: 2, 
+        rowSpan:2, 
+        chartXYOptions: { defaultAxisXTickStrategy: dateTimeTickStrategy }
+    })
+
+    // Create line series for total costs of the company.
+    const lineSeries = lineChart.addSplineSeries()
+    ```
+
+### API links
+
+* Dashboard: [Dashboard][]
+* XY cartesian chart: [ChartXY][]
+* Axis: [Axis][]
+* Axis tick strategies: [AxisTickStrategies][]
+* Auto-cursor modes: [AutoCursorModes][]
+* Segment series: [SegmentSeries][]
+* Freeform line series: [LineSeries][]
+* Spline series: [SplineSeries][]
+* Custom tick: [CustomTick][]
+* Solid LineStyle: [SolidLine][]
+* Empty LineStyle: [emptyLine][]
+* Solid FillStyle: [SolidFill][]
+* Empty FillStyle: [emptyFill][]
+* Empty TickStyle: [emptyTick][]
+* Color palettes: [ColorPalettes][]
+* UI Panel: [UIPanel][]
+* UI Layout builders: [UILayoutBuilders][]
+* UI Elements builders: [UIElementBuilders][]
+* Font settings: [FontSettings][]
+
+
+### Support
+
+If you notice an error in the example code, please open an issue on [GitHub][0] repository of the entire example.
+
+Official [API documentation][1] can be found on [Arction][2] website.
+
+If the docs and other materials do not solve your problem as well as implementation help is needed, ask on [StackOverflow][3] (tagged lightningchart).
+
+If you think you found a bug in the LightningChart JavaScript library, please contact support@arction.com.
+
+Direct developer email support can be purchased through a [Support Plan][4] or by contacting sales@arction.com.
+
+© Arction Ltd 2009-2019. All rights reserved.
+
+[0]: https://github.com/Arction/
+[1]: https://www.arction.com/lightningchart-js-api-documentation/
+[2]: https://www.arction.com
+[3]: https://stackoverflow.com/questions/tagged/lightningchart
+[4]: https://www.arction.com/support-services/
+
+[AutoCursorModes]: https://www.arction.com/lightningchart-js-api-documentation/v1.0.0/enums/autocursormodes.html
+[Axis]: https://www.arction.com/lightningchart-js-api-documentation/v1.0.0/classes/axis.html
+[AxisTickStrategies]: https://www.arction.com/lightningchart-js-api-documentation/v1.0.0/globals.html#axistickstrategies
+[ChartXY]: https://www.arction.com/lightningchart-js-api-documentation/v1.0.0/classes/chartxy.html
+[ColorPalettes]: https://www.arction.com/lightningchart-js-api-documentation/v1.0.0/globals.html#colorpalettes
+[CustomTick]: https://www.arction.com/lightningchart-js-api-documentation/v1.0.0/classes/customtick.html
+[Dashboard]: https://www.arction.com/lightningchart-js-api-documentation/v1.0.0/classes/dashboard.html
+[emptyFill]: https://www.arction.com/lightningchart-js-api-documentation/v1.0.0/globals.html#emptyfill
+[emptyLine]: https://www.arction.com/lightningchart-js-api-documentation/v1.0.0/globals.html#emptyline
+[emptyTick]: https://www.arction.com/lightningchart-js-api-documentation/v1.0.0/globals.html#emptytick
+[FontSettings]: https://www.arction.com/lightningchart-js-api-documentation/v1.0.0/classes/fontsettings.html
+[LineSeries]: https://www.arction.com/lightningchart-js-api-documentation/v1.0.0/classes/lineseries.html
+[SegmentSeries]: https://www.arction.com/lightningchart-js-api-documentation/v1.0.0/classes/segmentseries.html
+[SolidFill]: https://www.arction.com/lightningchart-js-api-documentation/v1.0.0/classes/solidfill.html
+[SolidLine]: https://www.arction.com/lightningchart-js-api-documentation/v1.0.0/classes/solidline.html
+[SplineSeries]: https://www.arction.com/lightningchart-js-api-documentation/v1.0.0/classes/splineseries.html
+[UIElementBuilders]: https://www.arction.com/lightningchart-js-api-documentation/v1.0.0/globals.html#uielementbuilders
+[UILayoutBuilders]: https://www.arction.com/lightningchart-js-api-documentation/v1.0.0/globals.html#uilayoutbuilders
+[UIPanel]: https://www.arction.com/lightningchart-js-api-documentation/v1.0.0/classes/uipanel.html
---
+++ src/index.js
@@ -0,0 +1,307 @@
+/**
+ * LightningChartJS example that showcases a business-like-Dashboard.
+ */
+// Import LightningChartJS
+const lcjs = require('@arction/lcjs')
+
+// Extract required parts from LightningChartJS.
+const {
+    lightningChart,
+    ColorPalettes,
+    SolidFill,
+    SolidLine,
+    UILayoutBuilders,
+    UIElementBuilders,
+    AutoCursorModes,
+    AxisTickStrategies,
+    emptyLine,
+    emptyTick,
+    emptyFill,
+    AxisScrollStrategies
+} = lcjs
+
+const {
+    createProgressiveTraceGenerator
+} = require('@arction/xydata')
+
+// Create styles for normal & selected graphs.
+const palette = ColorPalettes.arctionWarm(2)
+const mainStrokeStyle = new SolidLine().setFillStyle(new SolidFill().setColor(palette(0))).setThickness(20)
+const selectedFillStyle = new SolidFill().setColor(palette(1))
+
+// Department names 
+const teams = [
+    "Dev",
+    "Maintenance",
+    "Support",
+    "Sales",
+    "Marketing"
+]
+// 1 data-point per day
+const pointResolution = 24 * 60 * 60 * 1000
+// Generate data
+const budgets = Promise.all(
+    teams.map((_, index) => createProgressiveTraceGenerator()
+        .setNumberOfPoints(365)
+        .generate()
+        .toPromise()
+        .then(data => data.map(point => ({
+            x: point.x * pointResolution,
+            y: index > 0 ? Math.abs(point.y) * 100 + 100 : Math.abs(point.y) * 50 + 1800
+        })))
+    )
+)
+
+// Create dashboard which will host all chart and UI elements
+const db = lightningChart().Dashboard({
+    numberOfRows: 3,
+    numberOfColumns: 2
+})
+
+// Total 
+const totalBudgetsPerTeam = budgets.then(
+    teamBudgets =>
+        teamBudgets.map(budgetPerTeam =>
+            budgetPerTeam.reduce((sum, v) => sum + v.y, 0)
+        )
+)
+
+// Create Cartesian Chart for Bars
+const barChart = db.createChartXY({
+    columnIndex: 0,
+    rowIndex: 0,
+    columnSpan: 1,
+    rowSpan: 2
+})
+    // Disable auto cursor
+    .setAutoCursorMode(AutoCursorModes.disabled)
+    // Set correct chart title
+    .setTitle('Total expenses for 2018 per department')
+    // Disable mouse interactions
+    .setMouseInteractions(false)
+
+// Get Y axis
+const axisX = barChart.getDefaultAxisX()
+// Amount of pixels in chart area
+const axisXSize = axisX.scale.getCellSize()
+// Modify X axis
+axisX
+    // Disable default ticks.
+    .setTickStyle(emptyTick)
+    // Disable mouse interactions
+    .setMouseInteractions(false)
+    // Set correct range, so that is in pixel coordinates
+    .setInterval(0, axisXSize, false, true)
+    // Disable auto scaling
+    .setScrollStrategy(undefined)
+
+// Modify Y axis
+barChart.getDefaultAxisY().setTitle('Expenses ($)')
+    .setStrokeStyle(style => style.setThickness(0))
+    .setNibStyle(emptyLine)
+    .setMouseInteractions(false)
+// Create series for individual lines
+const bars = barChart.addSegmentSeries()
+// Calculate 
+const numberOfGapsBetweenBars = teams.length + 1
+// Create custom ticks to mark positions of different departments bars
+const customTicks = teams.map((team, i) => axisX
+    // Add new custom tick
+    .addCustomTick()
+    // Set team name as marker text
+    .setTextFormatter(_ => team)
+    // Position custom tick in according with department index
+    .setValue(axisXSize / numberOfGapsBetweenBars * (i + 1))
+    // Style marker of custom tick
+    .setMarker(marker => marker
+        // Change font settings
+        .setFont(fs => fs.setSize(12))
+        // Change stroke style
+        .setBackground(background => background
+            .setStrokeStyle(emptyLine)
+        )
+    )
+    // Disable gridstroke.
+    .setGridStrokeStyle(emptyLine)
+)
+
+
+// Decide on an origin for DateTime axes (shared between two charts).
+const dateOrigin = new Date(2018, 0, 1)
+const dateTimeTickStrategy = AxisTickStrategies.DateTime(dateOrigin)
+
+// Create chart for a single department costs distribution graph
+const lineChart = db.createChartXY({
+    columnIndex: 0, 
+    rowIndex: 2, 
+    columnSpan: 2, 
+    rowSpan: 1, 
+    chartXYOptions: { defaultAxisXTickStrategy: dateTimeTickStrategy }
+})
+    .setPadding({right:40})
+// Set the row height for the third row to take 50% of view space.
+db.setRowHeight(2, 2)
+// Create simple line series 
+const lineSeries = lineChart
+    .addLineSeries()
+    .setName('Total Expenses')
+    // Set selected fill color for the series
+    .setStrokeStyle((style) => style.setFillStyle(selectedFillStyle))
+
+// Style chart selected department costs distribution
+budgets.then(
+    costsOfTeams => {
+        // Finds the peak value across all departments
+        const max = costsOfTeams.reduce(
+            (max, costs) => costs.reduce(
+                (lMax, cost) => lMax > cost.y ? lMax : cost.y,
+                max
+            ),
+            0
+        )
+        // Get Y axis
+        lineChart
+            .getDefaultAxisY().setTitle('Expenses ($)')
+            // Disable auto scaling
+            .setScrollStrategy(AxisScrollStrategies.fitting)
+            // Set Y scale interval so that costs distribution fits
+            .setInterval(0, max)
+    }
+)
+
+lineSeries.setResultTableFormatter((builder, series, Xvalue, Yvalue) => {
+    // Find cached entry for the figure.
+    return builder
+        .addRow('Total expenses')
+        .addRow('Date: ' + series.axisX.formatValue(Xvalue))
+        .addRow('Expenses: $' + Yvalue.toFixed(2))
+})
+
+// Create interactive Bar chart 
+Promise.all([totalBudgetsPerTeam, budgets])
+    .then(([values, costsOfTeams]) => {
+        // Create bar for each department
+        // Departments are marked by custom ticks
+        const barCol = customTicks.map((tick, i) => {
+            // Get custom tick position
+            const pos = tick.getValue()
+            // Add Line which represents bar
+            // Line X position is based on custom tick value
+            return bars.add({
+                startX: pos,
+                startY: 0,
+                endX: pos,
+                endY: values[i]
+            })
+        })
+        // Create function which shows costs distribution per day for selected department
+        const selectedDepartment = i => {
+            // Change the chart title according to the selected department
+            lineChart.setTitle(`${teams[i]} expenses per day`)
+            // Remove points which belong to costs distribution of previously selected department
+            lineSeries.clear()
+            // Add points for costs distribution of newly selected department
+            lineSeries.add(costsOfTeams[i])
+            // Set main color to all bars
+            barCol.forEach(bar => bar.setStrokeStyle(mainStrokeStyle))
+            // Set special color for selected bar
+            barCol[i].setStrokeStyle((strokeStyle) => strokeStyle.setFillStyle(selectedFillStyle))
+        }
+        // Attach event listener for mouse/touch events of each bar 
+        barCol.forEach((bar, i) => {
+            bar.onMouseEnter(() => selectedDepartment(i))
+            bar.onTouchStart(() => selectedDepartment(i))
+        })
+        // Select the first department at initial value
+        selectedDepartment(0)
+    })
+
+// Draw text field with total amount of costs and description
+const column = db
+    // Create a dashboard without any content, 
+    // but with possibility to host any UI element
+    .createUIPanel({
+        columnIndex: 1,
+        rowIndex: 0,
+        columnSpan: 1,
+        rowSpan: 1
+    })
+    // Add a column structure to the UI panel
+    .addUIElement(UILayoutBuilders.Column)
+    .setPosition({ x: 50, y: 50 })
+    .setPadding({ right: 40 })
+
+totalBudgetsPerTeam.then(teamCosts => {
+    // Add the first row to the column
+    const firstRow = column.addElement(UILayoutBuilders.Row)
+    // Add a gap which allocates all empty space in front of text
+    firstRow.addGap()
+    // Add text element right after gap
+    firstRow.addElement(
+        UIElementBuilders.TextBox
+            // Modify TextBox builder to style the text field
+            .addStyler(textBox => textBox
+                // Define font settings for the text box
+                .setFont(fontSettings => fontSettings.setSize(75))
+                // Define content of the text box
+                .setText('$' + teamCosts.reduce((sum, cost) => sum + cost, 0).toFixed())
+            )
+    )
+    // Add a gap which allocates all empty space right after text
+    firstRow.addGap()
+    // Add a text box to the second row of the column
+    column.addElement(
+        UIElementBuilders.TextBox
+            // Modify TextBox builder to style the text field
+            .addStyler(textBox => textBox
+                .setFont(fontSettings => fontSettings.setSize(25))
+                .setText("Total company expenses")
+            )
+    )
+})
+
+// Draw total costs distribution per days
+const totalCostsChart = db
+    // Create a cartesian chart
+    .createChartXY({
+        columnIndex: 1, 
+        rowIndex: 1, 
+        columnSpan: 1, 
+        rowSpan: 1, 
+        chartXYOptions: { defaultAxisXTickStrategy: dateTimeTickStrategy }
+    })
+    // Specify ChartXY title
+    .setTitle('Total expenses per day')
+    .setPadding({ right: 40 })
+
+const totalCost = totalCostsChart
+    // Add the smooth line
+    .addSplineSeries()
+    .setName('Total Expenses ($)')
+    // Change the thickness of the stroke
+    .setStrokeStyle((strokeStyle) => strokeStyle.setThickness(2))
+
+budgets.then(teamBudgets => {
+    // Calculate total amount of costs per day
+    const totalCostsPerDays = new Array(365)
+    for (let i = 0; i < 365; i++) {
+        totalCostsPerDays[i] = {
+            x: i * pointResolution,
+            y: teams.reduce((sum, _, teamIndex) => sum + teamBudgets[teamIndex][i].y, 0)
+        }
+    }
+    // Draw a smooth line for total amount of costs per day
+    totalCost
+        // Hide points
+        .setPointFillStyle(emptyFill)
+        // Add data
+        .add(totalCostsPerDays)
+})
+totalCost.setResultTableFormatter((builder, series, Xvalue, Yvalue) => {
+    // Find cached entry for the figure.
+    return builder
+        .addRow('Total expenses')
+        .addRow('Date: ' + series.axisX.formatValue(Xvalue))
+        .addRow('Expenses: $' + Yvalue.toFixed(2))
+})
+totalCostsChart.getDefaultAxisY().setTitle('Expenses ($)')
